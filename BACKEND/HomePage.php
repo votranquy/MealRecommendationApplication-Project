263d478029523f8a40cdfcd9ce6a856bfd8b2575
votranquy <votranquy96@gmail.com>
2019-05-03 14:09:40 +0700

    fix load topfood

--- BACKEND/HomePage.php
+++ BACKEND/HomePage.php
@@ -7,9 +7,7 @@
   $conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("Can not connect to Database");
   mysqli_query($conn,"SET NAMES 'UTF8'");
   $sql = "SELECT * 
-  FROM FOOD
-  INNER JOIN FOOD_IMAGE
-  ON FOOD.id = FOOD_IMAGE.id_food";
+  FROM FOOD";
   // ORDER BY rate DESC
   
   $query = mysqli_query($conn,$sql);
@@ -19,36 +17,36 @@
     var $food_name;
     var $rate;
     var $address;
-    var $worktime;
-    var $img_path;
-    function Food($_id,$_food_name, $_rate, $_address, $_worktime, $_img_path){
+    //var $worktime;
+    var $image_path;
+    function Food($_id,$_food_name, $_rate, $_address, $_image_path){
       $this->food_name = $_food_name;
       $this->rate = $_rate;
       $this->address = $_address;
-      $this->worktime = $_worktime;
-      $this->img_path = $_img_path;
+      //$this->worktime = $_worktime;
+      $this->image_path = $_image_path;
       $this->id = $_id;
+
     }
   }
 
   
   $arrFood = array();
   while( $row = mysqli_fetch_array($query) ){
-    array_push($arrFood,new Food($row["id"],$row["food_name"],$row["rate"],$row["address"],$row["worktime"], $row["img_path"]));
+    array_push($arrFood,new Food($row["id"],$row["food_name"],$row["rate"],$row["address"], $row["image_path"]));
   }
 
   $the_number_of_items_per_page = 10;
   $page = $_GET["pagenumber"];
-  $newArrayFood = array();
+  
   $from = $page * $the_number_of_items_per_page;
-
+  $newArrayFood = array();
   try{
-  for($i=$from; ($i <= $from + $the_number_of_items_per_page - 1) && ($i< $max); $i = $i + 1){
-    array_push($newArrayFood,$arrFood[$i]);
+      for($i=$from; ($i <= $from + $the_number_of_items_per_page - 1) && ($i< $max); $i = $i + 1){
+        array_push($newArrayFood,$arrFood[$i]);
+      }
+    echo json_encode($newArrayFood);
+  }catch(Exception $e){
+    echo json_encode($newArrayFood);
   }
-
-  echo json_encode($newArrayFood);
-}catch(Exception $e){
-  echo "[]";
-}
 ?>
---
+++ api/saveData.php
@@ -0,0 +1,55 @@
+<?php 
+    //Server config
+    $server_username ="root"; 
+    $server_password =""; 
+    $server_host = "localhost"; 
+    $database = 'MealRecommendationApp'; 
+    $conn = mysqli_connect($server_host,$server_username,$server_password,$database);
+    mysqli_query($conn,"SET NAMES 'UTF8'");
+  
+    //Receive datas from frontend
+    $obj= json_decode(file_get_contents('php://input'),true);
+    $name         =$obj["name"];
+    $restaurantid =$obj["restaurantid"];
+    $address      =$obj["address"];
+    $category     =$obj["category"];
+    $latitude     =$obj["latitude"];
+    $longitude    =$obj["longitude"];
+    $rate         =$obj["rate"];
+    $first_image  =$obj["first_image"];
+
+    //check if this restaurant is exist
+    $sql_check = "SELECT * FROM FOOD
+                 WHERE restaurant_id = '$restaurantid' 
+                 ";
+    $sql_check_result = mysqli_query($conn,$sql_check);
+    
+    if(mysqli_num_rows($sql_check_result) == 1){
+    //Is exist. Update it.
+        $sql_update = 
+            "UPDATE FOOD 
+            SET 
+                food_name='$name', 
+                address='$address', 
+                category='$category', 
+                latitude='$latitude', 
+                longitude='$longitude',
+                rate='$rate', 
+                image_path='$first_image'
+            WHERE 
+                restaurant_id = '$restaurantid'";
+        $sql_update_result=mysqli_query($conn,$sql_update);
+        if($sql_update_result){echo("{\"result\":\"THANH_CONG\"}");}
+        else{echo("{\"result\":\"KHONG_THANH_CONG\"}");};
+    }
+    else{
+    //Is NOT exist. Insert it.
+        $sql_insert = 
+            "INSERT INTO FOOD(restaurant_id,food_name,address,category,latitude,
+            longitude,rate,image_path) 
+            VALUES('$restaurantid','$name','$address','$category','$latitude','$longitude','$rate','$image_path')";
+        $sql_insert_result=mysqli_query($conn,$sql_insert);
+        if($sql_insert_result){echo("{\"result\":\"THANH_CONG\"}");}
+        else{echo("{\"result\":\"KHONG_THANH_CONG\"}");};
+    }
+?>

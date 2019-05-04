586d7e937bff4c65bfefc1032e404c1ddf7c0407
votranquy <votranquy96@gmail.com>
2019-05-04 15:04:55 +0700

    fix Api change inf

--- BACKEND/HomePage.php
+++ BACKEND/HomePage.php
@@ -19,21 +19,24 @@
     var $address;
     //var $worktime;
     var $image_path;
-    function Food($_id,$_food_name, $_rate, $_address, $_image_path){
+    var $category;
+    var $restaurantid;
+    function Food($_id,$_food_name, $_rate, $_address, $_image_path,$_category,$_restaurantid){
       $this->food_name = $_food_name;
       $this->rate = $_rate;
       $this->address = $_address;
       //$this->worktime = $_worktime;
       $this->image_path = $_image_path;
       $this->id = $_id;
-
+      $this->category=$_category;
+      $this->restaurantid=$_restaurantid;
     }
   }
 
   
   $arrFood = array();
   while( $row = mysqli_fetch_array($query) ){
-    array_push($arrFood,new Food($row["id"],$row["food_name"],$row["rate"],$row["address"], $row["image_path"]));
+    array_push($arrFood,new Food($row["id"],$row["food_name"],$row["rate"],$row["address"], $row["image_path"], $row["category"], $row["restaurant_id"]));
   }
 
   $the_number_of_items_per_page = 10;

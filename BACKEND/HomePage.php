9cfe532fb41d16198b9fa47016ea8389c08f3e46
votranquy <votranquy96@gmail.com>
2019-05-21 14:16:23 +0700

    fix load null data

--- BACKEND/HomePage.php
+++ BACKEND/HomePage.php
@@ -6,13 +6,16 @@
   $database = 'MealApp'; 
   $conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("Can not connect to Database");
   mysqli_query($conn,"SET NAMES 'UTF8'");
+
   $sql = "SELECT * 
   FROM STORE
-  ORDER BY rate DESC";
-  // ORDER BY rate DESC
+  WHERE rate >= 4.5 AND rate <= 5.0";
+
+  //ORDER BY rate DESC
   
   $query = mysqli_query($conn,$sql);
-  $max = mysqli_num_rows($query) -1;
+  $max = mysqli_num_rows($query); //The number of result
+
   class Food{
     var $id;
     var $food_name;
@@ -39,22 +42,22 @@
   }
 
   
-  $arrFood = array();
+  $arrFood = array(); //0-> $max -1
   while( $row = mysqli_fetch_array($query) ){
     array_push($arrFood,new Food($row["id"],$row["food_name"],$row["rate"],$row["address"], $row["image_path"], $row["category"], $row["restaurant_id"], $row["menu"],$row["latitude"],$row["longitude"]));
   }
 
-  $the_number_of_items_per_page = 7;
+  $the_number_of_items_per_page = 10; //10 datas per page
   $page = $_GET["pagenumber"];
-  
   $from = $page * $the_number_of_items_per_page;
+  
+  
   $newArrayFood = array();
-  try{
-      for($i=$from; ($i <= $from + $the_number_of_items_per_page - 1) && ($i< $max-2); $i = $i + 1){
-        array_push($newArrayFood,$arrFood[$i]);
-      }
-    echo json_encode($newArrayFood);
-  }catch(Exception $e){
-    echo json_encode($newArrayFood);
-  }
+  if($from > $max){}
+  else{
+    for($i=$from; ($i <= $from + $the_number_of_items_per_page-1) && ($i <= $max-1); $i = $i + 1){
+      array_push($newArrayFood,$arrFood[$i]);
+    }  
+  } 
+  echo json_encode($newArrayFood);
 ?>

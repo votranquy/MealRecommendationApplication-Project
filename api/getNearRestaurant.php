c1f9f3b3183ca46d317b813e49a163a55a6635db
votranquy <votranquy96@gmail.com>
2019-06-01 21:32:48 +0700

    Add

--- api/getNearRestaurant.php
+++ api/getNearRestaurant.php
@@ -8,27 +8,30 @@ try{
 
   $sql_get = 
   "SELECT p.id, p.food_name, p.rate, p.address, p.image_path,
-  p.category, p.restaurant_id, p.latitude, p.longitude, p.menu, 
+  p.category, p.restaurant_id, p.latitude, p.longitude, 
+  -- p.menu, 
   q.name, q.image, 
-  ABS(SQRT((p.longitude-$yourlongitude)*(p.longitude-$yourlongitude)-(p.latitude-$yourlatitude)*(p.latitude-$yourlatitude))) as distance
+  -- ABS(SQRT((p.longitude-$yourlongitude)*(p.longitude-$yourlongitude)-(p.latitude-$yourlatitude)*(p.latitude-$yourlatitude))) as distance
+  ABS(p.longitude - $yourlongitude+p.latitude - $yourlatitude) as distance
   FROM STORE p LEFT JOIN FOOD q ON p.restaurant_id = q.restaurant_id
   WHERE rate > 0 
   AND (category='Quán ăn, ' OR category='Ăn vặt/vỉa hè, ' OR category='Café/Dessert, ' OR category='Ăn chay, ' OR category='Nhà hàng, ' OR category='Tiệm bánh, ') 
 --   AND NOT image = '/style/images/deli-dish-no-image.png'
 --   AND NOT distance = ''
   GROUP BY p.id
-  ORDER BY distance DESC
+  ORDER BY distance ASC 
+  -- DESC
   ";
   
 
   $topfood = $mysqli->query($sql_get);
-  $max  = $topfood->num_rows; //The number of result
-  
 
+  try{
+  $max  = $topfood->num_rows; //The number of result
   $arrFood = array(); //0-> $max -1
   while( $row = $topfood->fetch_assoc()){
        array_push($arrFood,$row);
-}
+  }
 
   $the_number_of_items_per_page = 10; //10 datas per page
   $page = $_GET["pagenumber"];
@@ -44,9 +47,15 @@ try{
   } 
   $result = array('result'=>'success','page'=>$page,'data'=>$newArrayFood);
   echo json_encode($result);
+  }
+  catch(Exception $e){ 
+    $result = array('result'=>'error');  
+       echo json_encode($result); 
+    // echo($e);
+  } 
 }catch(Exception $e){
-       // $result = array('result'=>'error');  
-       // echo json_encode($result);  
-       echo($e);
+       $result = array('result'=>'error');  
+       echo json_encode($result);  
+      //  echo($e);
 }  
 ?>
